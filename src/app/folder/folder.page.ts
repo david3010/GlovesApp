c131253721adca01bc199b2b03ba1aa6704f816f
David.Gallegos <david.gallegos@7-eleven.com.mx>
2020-02-29 19:58:03 -0600

    Demo Done

--- src/app/folder/folder.page.ts
+++ src/app/folder/folder.page.ts
@@ -8,10 +8,13 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class FolderPage implements OnInit {
   public folder: string;
-
+  public letras = [];
   constructor(private activatedRoute: ActivatedRoute) { }
 
   ngOnInit() {
+    this.letras = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H',
+    'I', 'J', 'K', 'L', 'M', 'N', 'Ñ', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
+  '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
     this.folder = this.activatedRoute.snapshot.paramMap.get('id');
   }
 
